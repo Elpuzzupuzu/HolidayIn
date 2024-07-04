e4235a32b928f636232fc250a2246d8fecc5874c
Mimit0s <yaelchiruiz97@gmail.com>
2024-07-04 13:20:16 -0600

    testing del carrito para el usuario

---
+++ Desktop/EC_BACK_MK2-master/EC_BACK_MK2-master/expendidora_spring-main/GatorGate/src/main/java/com/expendedora/GatorGate/Controller/CartController.java
@@ -0,0 +1,35 @@
+package com.expendedora.GatorGate.Controller;
+
+import com.expendedora.GatorGate.Model.Cart;
+import com.expendedora.GatorGate.Service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.expendedora.GatorGate.Model.Cart;
+import com.expendedora.GatorGate.Service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cart")
+public class CartController {
+
+    @Autowired
+    private CartService cartService;
+
+    @GetMapping("/{userId}")
+    public Cart getCartByUserId(@PathVariable Long userId) {
+        return cartService.getCartByUserId(userId);
+    }
+
+    @PostMapping("/add")
+    public void addItemToCart(@RequestParam Long userId, @RequestParam Long productId, @RequestParam Integer quantity) {
+        cartService.addItemToCart(userId, productId, quantity);
+    }
+
+    // Nuevo método para crear un carrito
+    @PostMapping("/create")
+    public Cart createCart(@RequestParam Long userId) {
+        return cartService.createCart(userId);
+    }
+}
